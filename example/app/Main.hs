fcc993783e5080c763e8cb6fb016509ce4310f00
Joel Bach <6702422+joel-bach@users.noreply.github.com>
2024-03-31 11:10:11 +0200

    Update example

--- example/app/Main.hs
+++ example/app/Main.hs
@@ -5,5 +5,5 @@ import StripeHandling
 
 main :: IO ()
 main = do
-  result <- getCheckoutSessionEvents
-  either (putStrLn . T.unpack) (mapM_ (putStrLn . T.unpack)) result
+  result <- getCheckoutSessionId
+  putStrLn result
